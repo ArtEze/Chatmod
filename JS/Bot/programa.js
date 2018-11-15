c8e80e3ca3597523a2fd9f348fb52ffa6ffa212d
Emiliano Ezequiel Parenti <passymas@gmail.com>
2018-11-15 01:33:20 -0300

    Bot: Ban a Agustín

--- JS/Bot/programa.js
+++ JS/Bot/programa.js
@@ -2023,6 +2023,7 @@ var ips_ban = [
 	,["190.183.212","BUDIN CON CHISPAS",44640]
 	,["185.233","-",44640]
 	,["185.234","-",44640]
+	,["186.134.90.106","Agustín",10080]
 ]
 var arrays = [
 	"votados", "mensajes", "entrar_salir", "idos", "entrados"
