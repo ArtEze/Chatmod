3241acde24fdb5d6ebcaa5f8d0a7c17ab010aa7c
Emiliano Ezequiel Parenti <passymas@gmail.com>
2019-01-27 23:18:21 -0300

    Cambiando activación del bot... Pasando de RawGit a GitHub Pages.

--- JS/Bot/Activar_Bot.js
+++ JS/Bot/Activar_Bot.js
@@ -14,12 +14,16 @@ window.cargar_cargador = function()
 {
 	if(document.querySelector(".chat")==null){return;}
 	window.dirección_bot = "https://github.com/ArtEze/Chatovod_Mod/blob/master/JS/Bot/Activar_Bot.js"
-	window.raw_dir = window.dirección_bot.replace(/^(.+)(git)(hub)(.+)(\/blob)(.+)(\/.+)(\.js)$/,"$1raw$2$4$6/")
+	window.dir_partes = JSON.parse(window.dirección_bot.replace(
+		/^(.+):\/\/(.+)\.([^/]+)\/(.+)\/(.+)\/blob\/master([^.]+)\/([^.]+)\.(.+)$/gi,'["$1","$2","$3","$4","$5","$6","$7","$8"]')
+	)
+	var a = window.dir_partes
+	window.raw_dir = a[0]+"://"+a[3].toLowerCase()+"."+a[1]+".io/"+a[4]+a[5]+"/"
 	var archivo = "Cargar_lista_archivos"
 	var interrogación = "?"
-	var ruta = window.raw_dir + archivo + ".js" + interrogación + window.número_aleatorio(1000)
-	var a = document.createElement("script")
-	a.src = ruta
-	document.head.appendChild(a)
+	var ruta = window.raw_dir + archivo + ".js" + interrogación + window.número_aleatorio(100000000000)
+	var b = document.createElement("script")
+	b.src = ruta
+	document.head.appendChild(b)
 }
 window.cargar_cargador()
