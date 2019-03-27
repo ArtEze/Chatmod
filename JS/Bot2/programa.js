80d9a55190f165bf39abeeeb230f8356415a1e1e
Emiliano Ezequiel Parenti <passymas@gmail.com>
2019-03-27 13:34:09 -0300

    Bot2: Activando deslizadores.

--- JS/Bot2/programa.js
+++ JS/Bot2/programa.js
@@ -1,3 +1,17 @@
 ﻿// Código hecho por Emiliano Ezequiel Parenti
 
-console.log("Hola")
+console.log("Cargado programa.js")
+
+function carga(){ 
+	var areas_mensajes = document.querySelectorAll(".chatMessagesTab")
+	Array.from(areas_mensajes).map(x=>{
+		var deslizador = x.querySelector(".chatMessagesScrollBar")
+		var deslizador_nuevo = x.querySelector(".chatMessagesContainer")
+		deslizador_nuevo.style["overflow-y"]="scroll"
+		if(deslizador!=null)
+		{
+			deslizador.remove()
+		}
+	})
+}
+carga()
