94af3da804bbccc7e2bf984d92c7b80d5d2802cf
ArtEze <passymas@gmail.com>
2019-09-04 20:18:52 -0300

    BotBot Cambios generales
    BotBot: Cambios generales. descargar a bajar pagina a página. Expresión regular para capturar li.

--- NodeJS/BotsVsBots/NodeChatovod_BotsVsBots_2.js
+++ NodeJS/BotsVsBots/NodeChatovod_BotsVsBots_2.js
@@ -5,51 +5,47 @@ Descargar http_otecald desde:
 node "/eze/Node/BotsVsBots/NodeChatovod_BotsVsBots_2.js" nekonwn 0
 */
 
-var http_otecald = require("http_otecald")
-var descargar = http_otecald.descargar_normal
+var bajar = require("http_otecald")
 
 var descargar_usuarios = function(){
 	var página = 0
 	var chat = "nekonwn"
 	var host = "http://"+chat+".chatovod.com"
 	var host_español = host + "/es/"
-	var url_1 = host_español+"users/?p="+(pagina*20)+"&s=0&_a=on&a=1"
+	var url_1 = host_español+"users/?p="+(página*20)+"&s=0&_a=on&a=1"
 
-	console.log(url_1)
-
-	var opciones = {
-		correcto: opciones.cbs.estado.correcto
-		, error: opciones.cbs.estado.error
-	}
-
-	descargar(url_1,"GET",opciones)
-	
-	descargar(url_1,"GET",callback({
-		headers: undefined,
-		callback_start: function(res){
-			console.log("aaaaaaaaaaaaa")
-			if(res.statusCode==302){
-				var y00 = res.headers.location
-				console.log("aaaaaaaaaaaaa")
-				comprobar_avatar_predeterminado(y00,id_cola,num_hilo,array_usuarios,usuario,resultado)
-				this.abort()
+	bajar.descargar(...bajar.objeto_a_pedido(bajar.retrollamadas({
+		url: url_1
+		, método: "GET"
+		, cabezas: {}
+		, cbs: {
+			estado: {
+				error: function(x){cb_err_analizar_usuarios(x)}
+				, correcto: function(x){cb_analizar_usuarios(x)}
 			}
-			return true
-		},
-		callback_end: function(x){
-			cb(x)
-			return true
-		},
-		respuesta: {
-			$: true,
-			sub: { start: true, cb_start: false, data: true,
-				end: {
-					$: true, analizar: { json: false, html: false }, cb_end: true }}},
-		res: {
-			$: true,
-			reg: { longitud: false, tipo: false, cookies: false, abortar: false }
-		},
-		error: true
-	}))
+		}, poderes: {
+			mostrar: {
+				$: true
+				, url: true
+				, cuerpo: {
+					error: false
+					, correcto: false
+				}
+			}, estado: {
+				redireccionar: true
+				, abortar_pedido: true
+			}
+		}
+	})))
+
 }
+function cb_analizar_usuarios(x){
+	console.log(x)
+	var usuarios = x.match(/<li.*?>.+?<\/li>/g)
+	console.log(usuarios)
+}
+function cb_err_analizar_usuarios(x){
+	console.log(x)
+}
+descargar_usuarios()
 
