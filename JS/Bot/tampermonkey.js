b22e06122421657bb8e137b7b609bedeeb7f407b
ArtEze <passymas@gmail.com>
2020-03-26 03:33:31 -0300

    Update tampermonkey.js

--- JS/Bot/tampermonkey.js
+++ JS/Bot/tampermonkey.js
@@ -1,4 +1,4 @@
-﻿// ==UserScript==
+// ==UserScript==
 // @name         Bot Otecald Chatovod
 // @version      2.7.6
 // @description  Cargador del cargador del Bot Otecald para Chatovod.
@@ -7,28 +7,15 @@
 // @grant        none
 // ==/UserScript==
 
-window.número_aleatorio = function(hasta){
-	return Math.floor(Math.random()*hasta)
-}
 window.cargar_cargador = function()
 {
 	if(document.querySelector(".chat")==null){return;}
 
-	/*
-		Tampermonkey
-		Archivo en proyecto: https://github.com/arteze/charlavod/blob/master/JS/Bot/tampermonkey.js
-		Ruta en GitHub Pages: https://arteze.github.io/charlavod/JS/Bot/tampermonkey.js
-	*/
-	window.dirección_bot = "https://github.com/arteze/charlavod/blob/master/JS/Bot/tampermonkey.js"
-
-	window.dir_partes = JSON.parse(window.dirección_bot.replace(
-		/^(.+):\/\/(.+)\.([^/]+)\/(.+)\/(.+)\/blob\/master([^.]+)\/([^.]+)\.(.+)$/gi,'["$1","$2","$3","$4","$5","$6","$7","$8"]')
-	)
-	var a = window.dir_partes
-	window.raw_dir = a[0]+"://"+a[3].toLowerCase()+"."+a[1]+".io/"+a[4]+a[5]+"/"
+	window.dirección_bot = "https://arteze.github.io/charlavod/JS/Bot/tampermonkey.js"
+	window.raw_dir = window.dirección_bot.split("/").slice(0,-1).concat("").join("/")
 	var archivo = "Cargar_lista_archivos"
 	var interrogación = "?"
-	var ruta = window.raw_dir + archivo + ".js" + interrogación + window.número_aleatorio(100000000000)
+	var ruta = window.raw_dir + archivo + ".js" + interrogación + Date.now()
 	var b = document.createElement("script")
 	b.src = ruta
 	document.head.appendChild(b)
