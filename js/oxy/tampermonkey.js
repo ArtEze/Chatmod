1214319f2b0abe1b775486ba8823f9ae61bbd938
ArtEze <passymas@gmail.com>
2020-03-29 21:38:16 -0300

    Cambios en el cargador de Oxy.

--- js/oxy/tampermonkey.js
+++ js/oxy/tampermonkey.js
@@ -1,40 +1,46 @@
 // ==UserScript==
-// @name         Bot Oxy para Chatovod
-// @version      2.7.7
+// @name		 Bot Oxy para Chatovod
+// @version	  2.7.7
 // @description  Cargador del bot Oxy para Chatovod.
-// @author       ArtEze
-// @match        *://*.chatovod.com/*
-// @exclude      *://*.chatovod.com/widget/rpframe
-// @exclude      *://account.chatovod.com/*
-// @grant        none
+// @author	   ArtEze
+// @match		*://*.chatovod.com/*
+// @exclude	  *://*.chatovod.com/widget/rpframe
+// @exclude	  *://account.chatovod.com/*
+// @grant		none
 // ==/UserScript==
 
-window.oxy = {
-	funciones: {}
-	, variables: {}
-}
+window.oxy = {}
 
 var oxy = window.oxy
-var funciones = oxy.funciones
 
-funciones.obtener_carpeta = function(url){
-	return url.split("/").slice(0,-1).concat("").join("/")
-}
-funciones.agregar_código = function(url){
-	var etiqueta = document.createElement("script")
-	etiqueta.src = url
-	document.head.appendChild(etiqueta)
-	return etiqueta
-}
-funciones.obtener_url_sin_cache = function(){
-	var devuelve = undefined
-	var url = oxy.url
-	var tampermonkey = url.tampermonkey
-	devuelve = tampermonkey.carpeta + tampermonkey.archivo + url.extensión + url.anticache
-	return devuelve
-}
-funciones.cargar = function()
+oxy.iniciar = function()
 {
+	oxy.funciones = {
+		cargar: {
+			obtener_url_sin_cache: function(){
+				var devuelve = undefined
+				var url = oxy.url
+				var tampermonkey = url.tampermonkey
+				devuelve = tampermonkey.carpeta + tampermonkey.archivo + url.extensión + url.anticache
+				return devuelve
+			}
+		}
+		, general: {
+			obtener_carpeta: function(url){
+				return url.split("/").slice(0,-1).concat("").join("/")
+			}
+			, agregar_código: function(url){
+				var etiqueta = document.createElement("script")
+				etiqueta.src = url
+				document.head.appendChild(etiqueta)
+				return etiqueta
+			}
+		}
+	}
+	oxy.variables = {
+		cargar: {}
+		, general: {}
+	}
 	oxy.url = {
 		tampermonkey: {
 			completo: "https://arteze.github.io/charlavod/js/oxy/tampermonkey.js"
@@ -42,19 +48,22 @@ funciones.cargar = function()
 		}
 		, interrogación: "?"
 		, punto: "."
-        , tipo_de_archivo: "js"
-    }
-    oxy.variables.cargar = {}
+		, tipo_de_archivo: "js"
+	}
 
-    var url = oxy.url
-    var variables = oxy.variables.cargar
+	var url = oxy.url
+	var variables = oxy.variables.cargar
+	var funciones = oxy.funciones
+	
+	oxy.funciones.general.cargar.iniciar = oxy.iniciar
+	delete oxy.iniciar
 
-    url.extensión = url.punto + url.tipo_de_archivo
+	url.extensión = url.punto + url.tipo_de_archivo
 	url.anticache = url.interrogación + Date.now()
 	url.tampermonkey.carpeta = funciones.obtener_carpeta(url.tampermonkey.completo)
 	url.tampermonkey.sin_cache = funciones.obtener_url_sin_cache()
 	variables.etiqueta_script = funciones.agregar_código(url.tampermonkey.sin_cache)
 }
 
-funciones.cargar()
+oxy.iniciar()
 
