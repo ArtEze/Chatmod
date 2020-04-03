7d1605542b37e88da8faca81603539787766907d
ArtEze <passymas@gmail.com>
2020-04-02 21:31:28 -0300

    Cambiando saludos.

--- js/oxy/programa.js
+++ js/oxy/programa.js
@@ -1529,8 +1529,8 @@ window.reestablecer_localStorage = function(){
 }
 window.separar_por_and = function(nombres){
 	var array = []
-	nombres.map(x=>array.push(...x.split(/[yiι]/i)))
-	array = array.filter(x=>x!="").reverse()
+	nombres.map(x=>array.push(...x.split(/\b[y]\b/i)))
+	array = array.filter(x=>x!="")//.reverse()
 	return array
 }
 window.unir_array_palabras = function(array,callback){
