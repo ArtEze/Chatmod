873271826c984c5ab51c8879dc1f99111541941f
ArtEze <passymas@gmail.com>
2020-04-06 18:05:04 -0300

    Cambio en Tampermonkey.

--- js/oxy/tampermonkey.js
+++ js/oxy/tampermonkey.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Bot Oxy para Chatovod
-// @version      2.8.2
+// @version      2.8.3
 // @description  Cargador del bot Oxy para Chatovod.
 // @author       ArtEze
 // @updateURL    https://arteze.github.io/charlavod/js/oxy/tampermonkey.js
@@ -14,9 +14,9 @@ window.oxy_tampermonkey = function(){
 	// La declaración con window permite borrarla. Además window es obligatorio en tampermonkey.
 
 	window.obtener_carpeta = url=>url.split("/").slice(0,-1).concat("").join("/")
-	window.carpeta = window.obtener_carpeta("https://arteze.github.io/charlavod/js/oxy/tampermonkey.js")
+	var carpeta = window.obtener_carpeta("https://arteze.github.io/charlavod/js/oxy/tampermonkey.js")
 	var etiqueta = document.createElement("script")
-	etiqueta.src = `${window.carpeta}iniciar.js?${Date.now()}`
+	etiqueta.src = `${carpeta}iniciar.js?${Date.now()}`
 	document.head.appendChild(etiqueta)
 }
 window.oxy_tampermonkey()
