8a275fa4483ab0c6a233462236acab9491cb866c
ArtEze <passymas@gmail.com>
2020-04-06 18:11:12 -0300

    Cambio en Tampermonkey.

--- js/oxy/tampermonkey.js
+++ js/oxy/tampermonkey.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Bot Oxy para Chatovod
-// @version      2.8.3
+// @version      2.8.4
 // @description  Cargador del bot Oxy para Chatovod.
 // @author       ArtEze
 // @updateURL    https://arteze.github.io/charlavod/js/oxy/tampermonkey.js
@@ -11,7 +11,11 @@
 // ==/UserScript==
 
 window.oxy_tampermonkey = function(){
-	// La declaración con window permite borrarla. Además window es obligatorio en tampermonkey.
+	/*
+		La declaración con window permite borrarla.
+		Tampermonkey necesita declarar variables de alguna manera.
+		No se usa "var oxy" porque de esa manera no sería accesible desde el tag "<script>".
+	*/
 
 	window.obtener_carpeta = url=>url.split("/").slice(0,-1).concat("").join("/")
 	var carpeta = window.obtener_carpeta("https://arteze.github.io/charlavod/js/oxy/tampermonkey.js")
