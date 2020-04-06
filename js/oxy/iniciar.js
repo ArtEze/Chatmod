91aa035573e7d41549992408ade98555cd570107
ArtEze <passymas@gmail.com>
2020-04-06 04:24:56 -0300

    Cambio en inicio del Tampermonkey.

--- js/oxy/iniciar.js
+++ js/oxy/iniciar.js
@@ -1,42 +1,53 @@
-window.iniciar_oxy = function iniciar_oxy(){
-	window.oxy = { x: true
-		, variables: {}
+iniciar_oxy = function iniciar_oxy(){
+	oxy = {
+		variables: {
+			archivo_actual: ["iniciar"]
+		}
 		, funciones: {
-			v: function(nombre,valor){
-				if(window.oxy.variables[nombre]==undefined){
-					window.oxy.variables[nombre] = []
+			tampermonkey: {
+				obtener_carpeta: obtener_carpeta
+			}
+			, iniciar: {
+				u: x.slice(-1)[0]
+				, v: function(nombre,valor){
+					var archivo = oxy.funciones.iniciar.u(oxy.variables.archivo_actual)
+					if(oxy.variables[archivo]==undefined){
+						oxy.variables[archivo] = {}
+					}
+					if(oxy.variables[archivo][nombre]==undefined){
+						oxy.variables[archivo][nombre] = []
+					}
+					oxy.variables[archivo][nombre].push([valor,Date.now()])
+					return oxy.variables[archivo][nombre]
 				}
-				window.oxy.variables[nombre].push([valor,Date.now()])
-				return window.oxy.variables[nombre]
-			}
-			, w: function(nombre){
-				return window.oxy.variables[nombre].slice(-1)[0][0]
-			}
-			, definir_esto: function(){
-				with(window.oxy.funciones){
-					v("iniciar_oxy_tampermonkey",window.iniciar_oxy)
-					delete window.iniciar_oxy
-					delete window.obtener_carpeta
+				, w: function(nombre){
+					return oxy.variables[archivo][nombre].slice(-1)[0][0]
 				}
-			}
-			, obtener_carpeta: window.obtener_carpeta
-			, agregar_código: function(url){
-				with(window.oxy.funciones){
-					var etiqueta = document.createElement("script")
-					v("tampermonkey_actual", url + ".js?" + Date.now())
-					etiqueta.src = w("tampermonkey_actual")
-					document.head.appendChild(etiqueta)
-					return etiqueta
+				, definir_esto: function(){
+					with(oxy.funciones){
+						v("iniciar_oxy_tampermonkey",iniciar_oxy)
+						delete iniciar_oxy
+						delete obtener_carpeta
+					}
+				}
+				, agregar_código: function(url){
+					with(oxy.funciones){
+						var etiqueta = document.createElement("script")
+						v("tampermonkey_actual", url + ".js?" + Date.now())
+						etiqueta.src = w("tampermonkey_actual")
+						document.head.appendChild(etiqueta)
+						return etiqueta
+					}
 				}
 			}
 		}
 	}
-	with(window.oxy.funciones){ // Usando with para mejor facilidad.
+	with(oxy.funciones){ // Usando with para mejor facilidad.
 		v("tampermonkey",document.querySelector("script[src*=github]").src)
 		v("carpeta",obtener_carpeta(w("tampermonkey")))
 		agregar_código(w("carpeta")+"cargar_lista_archivos")
 		definir_esto()
 	}
 }
-window.iniciar_oxy()
+iniciar_oxy()
 
