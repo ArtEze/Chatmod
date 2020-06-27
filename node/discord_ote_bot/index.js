2b5fa610e14508adb4727ca963c3dc437e69a23b
ArtEze <passymas@gmail.com>
2020-06-27 15:15:17 -0300

    Discord: Aceptando múltiples espacios.

--- node/discord_ote_bot/index.js
+++ node/discord_ote_bot/index.js
@@ -24,22 +24,14 @@ funs = {
 			return mensaje
 		}
 	},
-	quitar_espacios: function(mensaje)
-	{
-		var i=0
-		for(;i<mensaje.length;i++){
-			if( !(mensaje[i]==" "|mensaje[i]=="\t") ){break;}
-		}
-		return mensaje.slice(i)
-	},
 	quitar_prefijo: function(mensaje,prefijo){
-		var mensaje=this.quitar_espacios(mensaje)
-		
-		if( mensaje.slice(0,prefijo.length).toLowerCase()==prefijo ){
-			mensaje=mensaje.slice(prefijo.length)
+		var salida = ""
+		var regex = new RegExp("^\\s*"+prefijo+"\\s+","gi")
+		var prefijo_encontrado = mensaje.match(regex)
+		if(prefijo_encontrado!=null){
+			salida = mensaje.replace(regex,"")
 		}
-		var mensaje=this.quitar_espacios(mensaje)
-		return mensaje
+		return salida
 	},
 	desencriptar: function(token_encriptado,contraseña){
 		var mykey = crypto.createDecipher('aes-128-cbc', contraseña)
@@ -88,10 +80,10 @@ module.exports =
 					procesado = "Un mensaje."
 					break;
 				case "+":
-					procesado = enviar
+					procesado = args.slice(1).join(" ")
 					break;
 				default:
-					procesado = this.funs.procesar(enviar)
+					procesado = this.funs.procesar(post_pref)
 					break;
 			}
 			procesado = (procesado).toString()
