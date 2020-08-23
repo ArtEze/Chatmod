259455533ab6731cac263cbca8604ad16dac2c2b
ArtEze <passymas@gmail.com>
2020-08-23 18:49:59 -0300

    Bot 2: Bloquear... Vacío. Luakit.

---
+++ js/tampermonkey/bloquear_usuarios.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Bloqueador de usuarios para Chatovod
+// @version      1.0.0
+// @description  Bloqueador de usuarios para Chatovod.
+// @author       ArtEze
+// @updateURL    https://arteze.github.io/charlavod/js/tampermonkey/bloquear_usuarios.user.js
+// @match        *://*.chatovod.com/?bot=2
+// @exclude      *://*.chatovod.com/id*
+// @exclude      *://*.chatovod.com/register
+// @exclude      *://*.chatovod.com/users/*
+// @exclude      *://*.chatovod.com/widget/rpframe
+// @exclude      *://account.chatovod.com/*
+// @grant        none
+// ==/UserScript==
+
+
